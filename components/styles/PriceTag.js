14712f23a0d4456db304037f8992d21d0d863692
RedPhoenix <smellthemistyroses@gmail.com>
2021-05-25 16:58:50 +0530

    Tag adjustments

--- components/styles/PriceTag.js
+++ components/styles/PriceTag.js
@@ -7,7 +7,7 @@ const PriceTag = styled.span`
   font-weight: 600;
   padding: 5px;
   line-height: 1;
-  font-size: clamp(1.7rem, 2vw, 2.1rem);
+  font-size: clamp(1.6rem, 1.8vw, 2rem);
   display: inline-block;
   position: absolute;
   top: -3px;
