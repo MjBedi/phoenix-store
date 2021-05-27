c1593a64034ecdb4df7a219df77717a56134f632
RedPhoenix <smellthemistyroses@gmail.com>
2021-05-27 23:28:46 +0530

    Made product image click-throughable

--- components/Product.js
+++ components/Product.js
@@ -12,13 +12,17 @@ const Product = ({ product }) => {
 
   return (
     <ItemStyles>
-      <img
-        src={product?.photo?.image?.publicUrlTransformed}
-        alt={product.name}
-      />
+      <Link href={`/product/${product.id}`}>
+        <img
+          src={product?.photo?.image?.publicUrlTransformed}
+          alt={product.name}
+        />
+      </Link>
+
       <Title>
         <Link href={`/product/${product.id}`}>{product.name}</Link>
       </Title>
+
       <PriceTag> {formatMoney(product.price)} </PriceTag>
       <p> {product.description} </p>
       <div className="buttonList">
