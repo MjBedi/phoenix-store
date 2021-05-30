60fe73b4ba30394b57ff656fe973bdb9880abf21
RedPhoenix <smellthemistyroses@gmail.com>
2021-05-30 22:41:21 +0530

    Search dropdown adjustments

--- components/styles/DropDown.js
+++ components/styles/DropDown.js
@@ -3,6 +3,7 @@ import styled, { keyframes } from 'styled-components';
 const DropDown = styled.div`
   position: absolute;
   width: 90%;
+  max-width: var(--maxwidth);
   z-index: 2;
   /* border: 1px solid var(--lightgrey); */
   box-shadow: var(--bs);
@@ -15,6 +16,7 @@ const DropDown = styled.div`
 `;
 
 const DropDownItem = styled.div`
+  cursor: pointer;
   border-bottom: 1px solid var(--lightgrey);
   background: ${(props) => (props.highlighted ? '#f7f7f7' : 'white')};
   padding: 1rem;
@@ -41,15 +43,45 @@ const glow = keyframes`
 const SearchStyles = styled.div`
   position: relative;
   margin: 0.4rem 0;
-  input {
+
+  .inputBox {
     width: 95%;
     max-width: var(--maxwidth);
+    height: 100%;
+    position: relative;
+    border-radius: 1rem;
+    margin: 0 auto;
+    white-space: nowrap;
+    background: var(--litTwo);
+    /* &.loading {
+      animation: ${glow} 1s ease-in-out infinite alternate;
+    } */
+
+    svg {
+      height: 100%;
+      left: 12px;
+      position: absolute;
+      top: 9px;
+      fill: var(--lightpurple);
+    }
+  }
+
+  input {
+    width: 100%;
+    /* max-width: var(--maxwidth); */
     padding: clamp(1rem, 1vh, 2.6rem) clamp(1rem, 1.4vw, 2rem);
     border: 0;
     font-size: clamp(1.6rem, 1.8vw, 2rem);
     background: mistyrose;
+    height: 100%;
+    outline: none;
+    padding-inline-start: 52px;
     &.loading {
       animation: ${glow} 1s ease-in-out infinite alternate;
+    }
+
+    &:hover {
+      box-shadow: var(--bs);
     }
   }
 `;
