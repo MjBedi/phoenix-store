47c91f843342139b374b7c2012e6710b76a6a54f
RedPhoenix <smellthemistyroses@gmail.com>
2021-05-31 22:28:42 +0530

    UI and Functional changes

--- pages/reset.js
+++ pages/reset.js
@@ -1,11 +1,12 @@
 import RequestReset from '../components/RequestReset';
 import Reset from '../components/Reset';
+import { FormWrapStyles } from './signin';
 
 export default function ResetPage({ query }) {
   return (
-    <>
-      ResetPage {query.token}
+    <FormWrapStyles>
+      {/* ResetPage {query.token} */}
       {query.token ? <Reset token={query.token} /> : <RequestReset />}
-    </>
+    </FormWrapStyles>
   );
 }
