0d962ccd29f682e41bf1af8fc3e7e43c27d3093b
RedPhoenix <smellthemistyroses@gmail.com>
2021-06-02 16:40:41 +0530

    Responsive addendum @media

--- components/styles/Title.js
+++ components/styles/Title.js
@@ -1,14 +1,22 @@
 import styled from 'styled-components';
 
 const Title = styled.h3`
-  margin: 0 1rem;
+  margin: -1.8rem 1rem 0 1rem;
   text-align: center;
-  /* transform: skew(-5deg) rotate(-1deg); */
-  margin-top: -3rem;
   text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.1);
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow-x: clip;
+
+  @media (max-width: 484px) {
+    overflow: hidden;
+    white-space: normal;
+    text-overflow: ellipsis;
+
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+  }
 
   a {
     background: var(--primary);
@@ -17,7 +25,7 @@ const Title = styled.h3`
     font-size: clamp(1.7rem, 2vw, 2.1rem);
     text-align: center;
     color: white;
-    padding: 0 1rem;
+    padding: 0.1rem 1rem;
   }
 `;
 
