7acb0a3e937c746c669b1a1e5971613be02b51e7
RedPhoenix <smellthemistyroses@gmail.com>
2021-07-05 13:27:32 +0530

    Removed meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -15,21 +15,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en-US">
         <Head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="author" content="Mj" />
-          <meta
-            name="description"
-            content="Buy dark and milk Chocolate box, bar and pack."
-          />
-          <meta property="og:title" content="Phoenix Store" />
-          <meta property="og:description" content="Brief description" />
-          <meta property="og:site_name" content="Phoenix Store" />
-          {/* <meta property="og:image" content="/some-image.png" />
-          <meta property="og:url" content="/this-page.html" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:image:alt" content="image description" /> */}
-
           <link rel="icon" type="image/png" href="/static/favicon.png" />
 
           <script src="https://checkout.razorpay.com/v1/checkout.js" />
