47e14eb838181007fbb913fefcc6577c0375e32d
RedPhoenix <smellthemistyroses@gmail.com>
2021-10-07 11:42:36 +0530

    Updated prodendpoint

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://z99a69b55-z01afbaf1-gtw.qovery.io/api/graphql`;
+export const prodEndpoint = `https://phoenix-store-backend.herokuapp.com/api/graphql`;
 export const perPage = 9;
+// `https://z99a69b55-z01afbaf1-gtw.qovery.io/api/graphql`
